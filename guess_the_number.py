bcdd8fe153695524e60bdbb3fede247f7e891c7b
Christos Karavelas <66444801+chriskaravel@users.noreply.github.com>
2020-06-15 15:27:22 +0100

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,6 +1,6 @@
 score=[120,100,80,60,40,20,0]
-games_played=0
-high_score=0
+games_played= 0
+high_score= 0
 import os
 import json
 import random
@@ -8,7 +8,7 @@ import random
 class guess_number:
 
     def write_scores(self,player_name,score,games):
-        players = {"name":player_name,"score": score,"games": games}
+        players = {"name": player_name,"score": score,"games": games}
         with open('scores.json','a+') as file:
             json.dump(players,file) #insert data
             file.write('\n') #insert line
@@ -18,57 +18,57 @@ class guess_number:
         global games_played
         global score
         global high_score
-        games_played+=1
+        games_played+= 1
         number=random.randint(1,20)
-        turns=6
-        num_guesses=0
-        guess_correct=False
-        inserted_numbers_list=[]
-        while turns>0:
+        turns= 6
+        num_guesses= 0
+        guess_correct= False
+        inserted_numbers_list= []
+        while turns> 0:
             while True:
                 try:
                     print('Take a guess')
-                    guess = int(input()) 
+                    guess= int(input()) 
                     break
                 except ValueError:
-                    print("Oops!  That was no valid number.  Try again...")
-            turns-=1
-            if inserted_numbers_list.count(guess) >0:
+                    print("Oops! That was no valid number. Try again...")
+            turns-= 1
+            if inserted_numbers_list.count(guess)> 0:
                 print('You already inserted '+str(guess)+',try again')
-                turns+=1
+                turns+= 1
             else:
                 inserted_numbers_list.append(guess)
-                if guess>number:
-                    num_guesses+=1
+                if guess> number:
+                    num_guesses+= 1
                     print('Your guess is too high.')
-                elif guess<number:
-                    num_guesses+=1
+                elif guess< number:
+                    num_guesses+= 1
                     print('Your guess is too low.')
                 else:
-                    num_guesses+=1
+                    num_guesses+= 1
                     print('Good job, ' +player + '! You guessed my number in ' + str(num_guesses) + ' guesses!')
-                    high_score=high_score+score[num_guesses-1]
-                    guess_correct = True
+                    high_score= high_score+score[num_guesses-1]
+                    guess_correct= True
                     break
-        if guess_correct==False:
+        if guess_correct== False:
             print('Sorry. The number I was thinking of was ' + str(number))
-            high_score=high_score+score[num_guesses]
+            high_score= high_score+score[num_guesses]
             
     def __init__(self):
         super().__init__()
         answer = None
-        playerName=input('Hello!What is your name? \n')
-        print('Well, ' + playerName + ', I am thinking of a number between 1 and 20.You have 6 tries,good luck.')
-        self.game(playerName)
+        player_name= input('Hello!What is your name? \n')
+        print('Well, ' + player_name + ', I am thinking of a number between 1 and 20.You have 6 tries,good luck.')
+        self.game(player_name)
         while answer not in ('Y', 'N'):
-            answer = input('Do u want to play again Y/N ')
-            if answer.upper() == 'Y':
-                self.game(playerName)
-            elif answer.upper() == 'N':
+            answer= input('Do u want to play again Y/N ')
+            if answer.upper()== 'Y':
+                self.game(player_name)
+            elif answer.upper()== 'N':
                 answer='N'
-                self.write_scores(playerName,high_score,games_played)
+                self.write_scores(player_name,high_score,games_played)
             else:
                 print('Please enter Y or N.')
 
-x=guess_number()
+x= guess_number()
 input('Press ENTER to exit')
